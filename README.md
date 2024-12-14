1f4fd7ace7049ab6f3f245127742f1904ef96cc3
tiagoAbacaxi <tiago.blip.ai>
2024-12-14 00:04:11 -0300

    java

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # alurajavaSpringboot
-escalabilit
---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int idade = 18;
+
+        int salario = 1000;
+        
+
+
+    }
+}
